9172a339bfddc807cd3de02e3fa00d027c463135
Sebastian Heid <sebastian.heid@sap.com>
2019-03-14 18:35:50 +0100

    Make version more generic

--- Casks/synoptico.rb
+++ Casks/synoptico.rb
@@ -2,7 +2,7 @@ cask 'synoptico' do
   version 'v0.3.1'
   sha256 '0d9d3a5247c27335d1bdf3d661d031c2a5af598edb30659a80ad5e49ec2d3db0'
 
-  url 'https://github.com/friegger/synoptico/releases/download/v0.3.1/Synoptico-darwin-x64-0.3.1.zip'
+  url "https://github.com/friegger/synoptico/releases/download/#{version}/Synoptico-darwin-x64-0.3.1.zip"
   name 'Synoptico'
   homepage 'https://github.com/friegger/synoptico'
 
